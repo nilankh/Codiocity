4799812f8570c015d364f0f38b61be31fb17a040
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-06 09:48:04 +0530

    done all set up

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+var compiler = require('compilex');
+
+const app = express();
+app.use(bodyParser())
+
+
 
 const PORT = 5000
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
