6763563096568952953a2745ff3aebd491fcd3a8
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-06 09:51:51 +0530

    working on compilex

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const bodyParser = require('body-parser');
 var compiler = require('compilex');
 
 const app = express();
-app.use(bodyParser())
+// app.use(bodyParser())
+// this is deprecated.
 
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
+var options = { stats: true } //prints stats on console
+compiler.init(options)
 
 const PORT = 5000
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
